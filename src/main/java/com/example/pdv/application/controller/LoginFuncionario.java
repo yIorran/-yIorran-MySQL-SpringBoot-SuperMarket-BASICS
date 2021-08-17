989073377276f8a322e7ec5d4b368415c942604b
yIorran <iorran.gv@hotmail.com>
2021-08-17 15:00:30 -0300

    implementado atualização de funcionários @putmapping

--- src/main/java/com/example/pdv/application/controller/LoginFuncionario.java
+++ src/main/java/com/example/pdv/application/controller/LoginFuncionario.java
@@ -1,7 +1,26 @@
-package com.example.pdv.application.controller;
-
-public class LoginFuncionario {
-
-    
-
-}
+//package com.example.pdv.application.controller;
+//
+//import com.example.pdv.application.model.Funcionario;
+//import com.example.pdv.application.repository.FuncionarioRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/busca")
+//public class LoginFuncionario {
+//
+//    @Autowired
+//    FuncionarioRepository repositoryGen;
+//
+//    @PostMapping
+//    public @ResponseBody ResponseEntity<Object> varreduraFuncionario(@RequestBody Integer matricula, @RequestBody Integer senha) {
+//        Funcionario funcionario = new Funcionario(matricula, senha);
+//        if(repositoryGen.equals(funcionario)){
+//            System.out.println("Done");
+//        }
+//        return new ResponseEntity<Object>(repositoryGen.findAll(), HttpStatus.OK);
+//    }
+//}
--- src/main/java/com/example/pdv/application/model/Funcionario.java
+++ src/main/java/com/example/pdv/application/model/Funcionario.java
@@ -1,8 +1,11 @@
 package com.example.pdv.application.model;
 
+import com.example.pdv.application.repository.FuncionarioRepository;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,7 +22,15 @@ public class Funcionario {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer Id;
 
+    private String nome;
     private Integer matricula;
     private Integer senha;
+
+
+    public Funcionario(String nome, Integer matricula, Integer senha) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.senha = senha;
+    }
 
 }
